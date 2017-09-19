c156a9ca6c0a142ed6b53a36600009ac93611ea3
Casey DeRoo <Casey@Caseys-MacBook-Pro.local>
2017-09-19 14:01:24 -0400

    Stashed commit prior to optics merger

--- SPOPetal.py
+++ SPOPetal.py
@@ -12,7 +12,7 @@ import PyXFocus.grating as grat
 import PyXFocus.conicsolve as conic
 import PyXFocus.transformMod as transM
 
-import arcusTrace.ParamFiles.arcus_params_rev1p4 as cfpar
+import arcusTrace.ParamFiles.arcus_params_rev1p5 as cfpar
 
 ####################################################################
 # SPO-Related Functions
@@ -99,110 +99,26 @@ def slope_trans(rays,angle):
     rays[4],rays[5],rays[6] = new_slopes[0,:],new_slopes[1,:],new_slopes[2,:]
     return
 
-#def SPOPetalTrace(rays):
-#    for i in [0,1]:#range(cfpar.N_xous):
-#        # Produces the selection of rays hitting a particular (the "ith") XOU.
-#        xou_rays = SPO_ray_select(rays,cfpar.xou_loc_irs[i],cfpar.xou_loc_ors[i],cfpar.xou_widths[i],cfpar.xou_cangles[i])
-#        
-#        # Clocking the rays at the SPO clock angle.
-#        clocked_rays = tran.copy_rays(xou_rays)
-#        tran.transform(clocked_rays,0.,0.,0.,0.,0.,-cfpar.xou_cangles[i]*pi/180)
-#    
-#        pdb.set_trace()
-#        # Now moving the rays to concide with the optical axis of this XOU.
-#        tran.transform(clocked_rays,0,cfpar.xou_rad_offsets[i],0,cfpar.xou_tilts[i],0,0)
-#
-#        pdb.set_trace()
-#        # Performing the trace through the SPO under `square on' incidence conditions, where the rays may be at a tilt set
-#        # by the cfpar.xou_tilts. 
-#        thru_rays = SPOtrace(clocked_rays,rin = cfpar.xou_opt_irs[i],rout = cfpar.xou_opt_ors[i],azwidth = cfpar.xou_widths[i])
-#        
-#        # Undoing this transform to get to the XOU axis.
-#        tran.transform(thru_rays,0,0,0,-cfpar.xou_tilts[i],0,0)
-#        tran.transform(thru_rays,0,-cfpar.xou_rad_offsets[i],0,0,0,0)
-#        
-#        # Reversing the clock angle and tracing the rays to the focal plane.
-#        tran.transform(thru_rays,0.,0.,-cfpar.F,0.,0.,cfpar.xou_cangles[i]*pi/180)
-#        
-#        if i == 0:
-#            spo_rays = thru_rays
-#            xou_hit = ones(len(spo_rays[0]))*i
-#        else:
-#            spo_rays = hstack((spo_rays,thru_rays))
-#            xou_hit = hstack((xou_hit,ones(len(thru_rays[0]))*i))
-#    return spo_rays,xou_hit,clocked_rays
-
-#def SPOPetalTrace(rays):
-#    for i in [0,1]:#range(cfpar.N_xous):
-#        # Produces the selection of rays hitting a particular (the "ith") XOU.
-#        xou_rays = SPO_ray_select(rays,cfpar.xou_loc_irs[i],cfpar.xou_loc_ors[i],cfpar.xou_widths[i],cfpar.xou_cangles[i])
-#        
-#        # Clocking the rays at the SPO clock angle.
-#        clocked_rays = tran.copy_rays(xou_rays)
-#        tran.transform(clocked_rays,0.,0.,0.,0.,0.,-cfpar.xou_cangles[i]*pi/180)
-#    
-#        # Now moving the rays to concide with the optical axis of this XOU.
-#        tran.transform(clocked_rays,0,cfpar.xou_rad_offsets[i],0,0,0,0)
-#        
-#        pdb.set_trace()
-#        slope_trans(clocked_rays,cfpar.xou_tilts[i])
-#        pdb.set_trace()
-#        
-#        # Performing the trace through the SPO under `square on' incidence conditions, where the rays may be at a tilt set
-#        # by the cfpar.xou_tilts. Note that this rotation is about the origin (on optical axis, !NOT! about the module (so that )
-#        thru_rays = SPOtrace(clocked_rays,rin = cfpar.xou_opt_irs[i],rout = cfpar.xou_opt_ors[i],azwidth = cfpar.xou_widths[i])
-#        
-#        # Undoing this transform to get to the XOU axis.
-#        #slope_trans(thru_rays,cfpar.xou_tilts[i]*2)
-#        tran.transform(thru_rays,0,-cfpar.xou_rad_offsets[i],0,0,0,0)
-#        
-#        # Reversing the clock angle and tracing the rays to the focal plane.
-#        tran.transform(thru_rays,0.,0.,-cfpar.F,0.,0.,cfpar.xou_cangles[i]*pi/180)
-#        
-#        if i == 0:
-#            spo_rays = thru_rays
-#            xou_hit = ones(len(spo_rays[0]))*i
-#        else:
-#            spo_rays = hstack((spo_rays,thru_rays))
-#            xou_hit = hstack((xou_hit,ones(len(thru_rays[0]))*i))
-#    return spo_rays,xou_hit,clocked_rays
-
-def SPOPetalTrace(rays):
-    for i in [1]: #range(cfpar.N_xous):
+def SPOPetalTrace(rays,module_range = range(cfpar.N_xous)):
+    for i in module_range:
         # Produces the selection of rays hitting a particular (the "ith") XOU.
-        #xou_rays = SPO_ray_select(rays,cfpar.xou_loc_irs[i],cfpar.xou_loc_ors[i],cfpar.xou_widths[i],cfpar.xou_cangles[i])
+        xou_rays = SPO_ray_select(rays,cfpar.xou_irs[i],cfpar.xou_ors[i],cfpar.xou_widths[i],cfpar.xou_cangles[i])
         
         # Clocking the rays at the SPO clock angle.
         clocked_rays = tran.copy_rays(xou_rays)
         tran.transform(clocked_rays,0.,0.,0.,0.,0.,-cfpar.xou_cangles[i]*pi/180)
-    
-        # Now moving the rays to coincide with the center of of this XOU, which is a combination of its
-        # radial offset and its central radius. We then tilt the module about its center by its particular
-        # XOU tilt. The initial translation to the center of the XOU is done so that the tilt is performed
-        # about the center of the XOU -- this better preserves the intersection of the rays with the SPO.
-        r_cen = mean([cfpar.xou_loc_irs[i],cfpar.xou_loc_ors[i]])
-        tran.transform(clocked_rays,0,r_cen,0,cfpar.xou_tilts[i],0,0)
 
-        pdb.set_trace()
-
-        # Next, we translate the rays back so that they are referenced to the optical axis of the XOU, and are traced through.
-        tran.transform(clocked_rays,0,-r_cen + cfpar.xou_rad_offsets[i],0,0,0,0)
-        
-        # Performing the trace through the SPO under `square on' incidence conditions, where the rays may be at y slope set
-        # by the cfpar.xou_tilts.
-        thru_rays = SPOtrace(clocked_rays,rin = cfpar.xou_opt_irs[i],rout = cfpar.xou_opt_ors[i],azwidth = cfpar.xou_widths[i])
-        
-        # Undoing this transform to get to the XOU axis.
-        tran.transform(thru_rays,0,0,0,-cfpar.xou_tilts[i],0,0)
-        tran.transform(thru_rays,0,-cfpar.xou_rad_offsets[i],0,0,0,0)
+        # Performing the trace through the SPO under `square on' incidence conditions, where the rays may be at a tilt set
+        # by the cfpar.xou_tilts. 
+        thru_rays = SPOtrace(clocked_rays,rin = cfpar.xou_irs[i],rout = cfpar.xou_ors[i],azwidth = cfpar.xou_widths[i])
         
         # Reversing the clock angle and tracing the rays to the focal plane.
         tran.transform(thru_rays,0.,0.,-cfpar.F,0.,0.,cfpar.xou_cangles[i]*pi/180)
         
-        if i == 0:
+        if i == module_range[0]:
             spo_rays = thru_rays
             xou_hit = ones(len(spo_rays[0]))*i
         else:
             spo_rays = hstack((spo_rays,thru_rays))
             xou_hit = hstack((xou_hit,ones(len(thru_rays[0]))*i))
-    return spo_rays,xou_hit,clocked_rays
+    return spo_rays,xou_hit
