b6733dc5c15d7f61665a274eabd35fda6bd95527
Casey DeRoo <cderoo@uiowa.edu>
2019-09-26 09:08:22 -0500

    Casey Local

--- SPOPetalWeighted.py
+++ SPOPetalWeighted.py
@@ -254,6 +254,7 @@ def SPOPetalTrace(ray_object,xou_dict):
     for key in xou_dict.keys():
         ray_ind_this_xou = ray_object.xou_hit == xou_dict[key].xou_num
         xou_ray_object = ray_object.yield_object_indices(ind = ray_ind_this_xou)
+        pdb.set_trace()
         if size(xou_ray_object.x) != 0:
             try:
                 petal_ray_dict[key] = XOUTrace(xou_ray_object,xou_dict[key])
