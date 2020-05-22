1694fea97d4e47bcf776776c2e59ae35dfda6eab
Casey DeRoo <cderoo@uiowa.edu>
2020-05-21 20:05:37 -0500

    Changed XOU reflectivity in pointers

--- ParamFiles/pointers.py
+++ ParamFiles/pointers.py
@@ -6,4 +6,4 @@ default_diff_eff_pointer = 'C:/Users/Casey/Software/Bitbucket/caldb-inputdata/gr
 # These need to point to something in the Bitbucket.
 reflib_directory = 'C:/Users/Casey/Software/ReflectLib'
 default_det_pointer = 'C:/Users/Casey/Software/python_repository/arcusTrace/ParamFiles/Arcus_DetectorArray_Specs_Rev3p0_171211.csv'
-default_xou_ref_pointer = 'C:/Users/Casey/Software/ReflectLib/SL_LSi02_LThick1p0_LRough0p4_SSi_SRough0p0_Pol1_CXRO.npy'
+default_xou_ref_pointer = 'C:/Users/Casey/Software/ReflectLib/HighRes_SL_LSiO2_LThick400p0_LRough0p4_SSi_SRough0p0_Pol1_CXRO.npy'
